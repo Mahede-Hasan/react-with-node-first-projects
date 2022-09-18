8fc36428869f2955f359590140e8294fe33d9ab9
Mahede-Hasan <mahedehasan.cd91@gmail.com>
2022-09-17 23:59:33 -0700

    react with node another backend file name intro node

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
+
+  const handleAddUser = event => {
+    event.preventDefault()
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = {name, email}
+    fetch('http://localhost:5000/user', {
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+  .then(res => res.json())
+  .then(data => {
+    const newUser = [...users, data];
+    setUsers(newUser)
+  })
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My own users {users.length}</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder='name' required />
+        <input type="email" name="email" placeholder='Email' required id="" />
+        <input type="submit" value="Add User" />
+      </form>
+      {
+        users.map(us => <li>id: {us.id} name:{us.name} email: {us.email} </li>)
+      }
     </div>
   );
 }
